ae7879294cf47c53f214271a102ca4772d5d7c9e
Ananya Apoorva <ananya.apoorva28@gmail.com>
2026-07-29 22:21:42 +0530

    Time: 85 ms (14.65%), Space: 113.5 MB (8.56%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int>s;
-        for(int i=0;i<nums.size();i++){
-            if(s.count(nums[i])){
-                return true;
-            }
-            s.insert(nums[i]);
-        }
-        return false;
+        unordered_set<int>s(nums.begin(),nums.end());
+        return (s.size()<nums.size());
     }
 };
