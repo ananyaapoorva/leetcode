c2eac181ed1ed73e9b5a2d196ba0b5b40478213f
Ananya Apoorva <ananya.apoorva28@gmail.com>
2026-07-30 23:19:34 +0530

    Time: 7 ms (18.64%), Space: 9.7 MB (76.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<char>v1={s.begin(),s.end()};
-        vector<char>v2={t.begin(),t.end()};
-
-        sort(v1.begin(),v1.end());
-        sort(v2.begin(),v2.end());
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
         
-        if(v1==v2){
-            return true;
-        }
-        return false;
+        return (s==t);
     }
 };
