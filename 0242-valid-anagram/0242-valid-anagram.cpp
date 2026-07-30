70d1a673c8fda8f14889c9b594df7ca050891f8d
Ananya Apoorva <ananya.apoorva28@gmail.com>
2026-07-30 23:29:09 +0530

    Time: 0 ms (100%), Space: 9.9 MB (11.6%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        if(s.length()!=t.length())
+            return false;
+
+        unordered_map<char,int>countS, countT;
+        for(int i=0;i<s.length();i++){
+            countS[s[i]]++;
+            countT[t[i]]++;
+        }
         
-        return (s==t);
+        return (countS==countT);
     }
 };
