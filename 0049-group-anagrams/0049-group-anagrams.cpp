69d4a877607b8c18e5d1dd87f451f6f4b64550ec
Ananya Apoorva <ananya.apoorva28@gmail.com>
2026-08-02 22:59:23 +0530

    Time: 10 ms (93.26%), Space: 23.9 MB (94.14%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>>ans;
-        vector<string>copy(strs.begin(),strs.end());
-        for(int i=0;i<copy.size();i++){
-            sort(copy[i].begin(),copy[i].end());
+        unordered_map<string, vector<string>> groups;
+        
+        for (const string& s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            groups[key].push_back(s);
         }
-        vector<bool>check(copy.size(),false);
-        for(int i=0;i<copy.size();i++){
-            if(check[i])
-                continue;
-            vector<string>v;
-            v.push_back(strs[i]);
-            for(int j=i+1;j<copy.size();j++){
-                if(check[j])
-                    continue;
-                if(copy[i]==copy[j]){
-                    v.push_back(strs[j]);
-                    check[j]=true;
-                }
-            }
-            ans.push_back(v);
+        
+        vector<vector<string>> ans;
+        for (auto& [key, group] : groups) {
+            ans.push_back(move(group));
         }
         return ans;
     }
