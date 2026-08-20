fd132072fcab51a4cc493bc60c6f426ab2dfd054
Ananya Apoorva <ananya.apoorva28@gmail.com>
2026-08-20 19:08:59 +0530

    Time: 0 ms (100%), Space: 10 MB (35.88%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string s1="";
-        for(auto& c:s){
-            if(isalnum(c))
-                s1+=tolower(c);
-        }
+        // string s1="";
+        // for(auto& c:s){
+        //     if(isalnum(c))
+        //         s1+=tolower(c);
+        // }
 
-        int i=0,j=s1.size()-1;
+        int i=0,j=s.size()-1;
         while(i<=j){
-            if(s1[i]!=s1[j])
+            if(!isalnum(s[i])){
+                i++;
+                continue;
+            }
+            if(!isalnum(s[j])){
+                j--;
+                continue;
+            }
+            if(tolower(s[i])!=tolower(s[j]))
                 return false;
             i++;
             j--;
